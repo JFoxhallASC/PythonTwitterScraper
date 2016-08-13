442ce961903c192240a33de1e6a938cf8bdca7d0
JFoxhall <jfoxhall99@gmail.com>
2016-08-13 10:05:25 -0400

    twitter bot
    will find tweet and give id. Bug: only shows one tweet.

---
+++ firebasetest.py
@@ -0,0 +1,30 @@
+#from firebase import firebase
+from main import *
+# config = {
+#   "apiKey": "AIzaSyCK1lD1GXri_mI-1KeNk2-1cMcxZxKTKkU",
+#   "authDomain": "tweetsweep-d70a2.firebaseapp.com",
+#   "databaseURL": "https://tweetsweep-d70a2.firebaseio.com",
+#   "storageBucket": "tweetsweep-d70a2.appspot.com"
+# }
+
+
+# r = api.request('search/tweets', {'q':'#tweetsweep679'})
+# for item in r:
+#     print(item['coordinates'])
+#     print(item['id'])
+#     print(item['text'])
+
+# firebase = firebase.FirebaseApplication('https://your_storage.firebaseio.com', None)
+# new_user = 'Ozgur Vatansever'
+#
+# result = firebase.post('/users', new_user, {'print': 'pretty'}, {'X_FANCY_HEADER': 'VERY FANCY'})
+# print result
+# {u'name': u'-Io26123nDHkfybDIGl7'}
+#
+# result = firebase.post('/users', new_user, {'print': 'silent'}, {'X_FANCY_HEADER': 'VERY FANCY'})
+# print result == None
+# True
+
+print place
+print content
+print thing
--- main.py
+++ main.py
@@ -2,14 +2,20 @@
 from TwitterAPI import TwitterAPI
 api = TwitterAPI('HaRkHchUXROSHDqvBbynCsyDc', 'vs7vn4JFpV4VIq90LcwRaepB8yJQWitxEGyTzrn8HsNNxns962', '763153901323419649-gNJRtqOH7aSQ0bqqCaPkDC1ZoeJqqid', 'Xe5eGwZ9buxvDBBKJbwGKuQC3ADsw21bg5QKAQBRyxuVL')
 
-
 r = api.request('search/tweets', {'q':'#tweetsweep679'})
+
+place = 'place'
+thing = 'thing'
+content = 'content'
+
 for item in r:
-    print(item['coordinates'])
-    print(item['id'])
-    place = item['coordinates']
-    thing = item['id']
+    # print(item['coordinates'])
+    # print(item['id'])
+    # print(item['text'])
+    place = (item['coordinates'])
+    thing = (item['id'])
+    content = (item['text'])
 
-
-def sendToFirebase(place, thing):
-    
+print place
+print content
+print thing
